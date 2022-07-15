58a7ab5305c3ba7ac89ee635b74a5749d431cd03
z9fr <z9fr@protonmail.com>
2022-07-15 10:18:28 +0530

    feat: display team details name, logo and other names

--- pkg/handler/teamDetails.go
+++ pkg/handler/teamDetails.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"fmt"
+	"github.com/fatih/color"
 
 	"github.com/sirupsen/logrus"
 	"github.com/z9fr/ctftime-cli/pkg/utils"
@@ -14,6 +15,16 @@ func TeamDetails(teamID string) {
 	if err != nil {
 		logrus.Fatal(err)
 	}
+
+	whilte := color.New(color.FgWhite)
+	boldWhite := whilte.Add(color.Underline)
+	boldWhite.Println(fmt.Sprintf("\n%s | %s\n", ctfTeamInfo.Name, ctfTeamInfo.Country))
+
+	fmt.Println("Aliases")
+	for _, a := range ctfTeamInfo.Aliases {
+		fmt.Printf("- %s\n", a)
+	}
+	fmt.Printf("\n%s \n\n", ctfTeamInfo.Logo)
 
 	utils.CreateGraph(ctfTeamInfo)
 
